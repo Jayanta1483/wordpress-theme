8690effa790dadfd51ac983dc53d0e4fd30fbd28
Jayanta <nemojoy2001@gmail.com>
2021-03-05 19:23:28 +0530

    carousel indicator dynamic

--- theme-2/front-page.php
+++ theme-2/front-page.php
@@ -1,63 +1,5 @@
 <?php get_header(); ?>
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-        <?php
-        $category_id = array('include' => '8, 24, 19');
-        $categories = get_categories($category_id);
-        $count = 0;
-        foreach ($categories as $category) {
-            $arg = array(
-                'cat' => $category->term_id,
-                'post_type' => 'post',
-                'posts_per_page' => 1,
-                'category__not_in' => 1
 
-            );
-
-            $last_blog = new WP_Query($arg);
-
-            if ($last_blog->have_posts()) {
-                while ($last_blog->have_posts()) {
-                    $last_blog->the_post(); ?>
-
-                    <div class="carousel-item <?php echo ($count == 0) ? 'active' : '';  ?>">
-                        <img src="<?php the_post_thumbnail_url('full'); ?>" class="d-block w-100" height="350" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5><?php the_title();  ?></h5>
-                        </div>
-                    </div>
-
-        <?php $count++;
-                }
-            }
-
-            wp_reset_postdata();
-        }
-        ?>
-        <!-- <div class="carousel-item active">
-            <img src="" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="" class="d-block w-100" alt="...">
-        </div>
-        <div class="carousel-item">
-            <img src="" class="d-block w-100" alt="...">
-        </div> -->
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div>
 
 <article style="background: <?php echo background_color(); ?>;">
 
@@ -110,29 +52,60 @@
 
     </div>
 
-    <!-- <div class="col-xs-12 col-sm-4">
-<?php
-// $arg = array(
-//     'post_type'=>'post',
-//     'posts_per_page'=>1,
-//     'offset'=>1
-// );
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
 
-// $last_blog = new WP_Query($arg);
+<div class="carousel-inner">
+    <?php
+    $category_id = array('include' => '8, 24, 19, 3');
+    $categories = get_categories($category_id);
+    $count = 0;
+    $indicator = '';
+    foreach ($categories as $category) {
+        $arg = array(
+            'cat' => $category->term_id,
+            'post_type' => 'post',
+            'posts_per_page' => 1,
+            'category__not_in' => 1
 
-//     if($last_blog->have_posts()){
-//         while($last_blog->have_posts()){
-//             $last_blog->the_post();
-//             get_template_part('template-parts/content', get_post_format());
-//         }
-//     }
+        );
 
-//     wp_reset_postdata();
+        $last_blog = new WP_Query($arg);
 
-?>
+        if ($last_blog->have_posts()) {
+            while ($last_blog->have_posts()) {
+                $last_blog->the_post(); ?>
 
+                <div class="carousel-item <?php echo ($count == 0) ? 'active' : '';  ?>">
+                    <img src="<?php the_post_thumbnail_url('full'); ?>" class="d-block w-100" height="350" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5><?php the_title();  ?></h5>
+                    </div>
+                </div>
+
+    <?php 
+      $active = ($count == 0) ? 'active' : '';
+      $indicator .= '<li data-target="#carouselExampleIndicators" data-slide-to="'.$count.'" class="'.$active.'"></li>';
+      
+      $count++;
+            }
+        }
+
+        wp_reset_postdata();
+    }
+    ?>
 </div>
-</div> -->
+<ol class="carousel-indicators">
+    <?php  echo $indicator;    ?>
+</ol>
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+</a>
+</div>
 
 
     <?php get_footer(); ?>
--- theme-2/template-parts/content-featured.php
+++ theme-2/template-parts/content-featured.php
@@ -1,11 +1,12 @@
 
 
-<article class="text-center">
+<article class="text-center shadow p-3 mb-5 bg-white rounded">
     <?php if (has_post_thumbnail()) {  ?>
         
             <img src="<?php the_post_thumbnail_url('thumbnail');  ?>" class="img-thumbnail" alt="">
     
     <?php } ?>
     <?php the_title(sprintf('<h3><a href="%s">', esc_url(get_permalink())), '</a></h3>');   ?>
-    <small><?php the_category(); ?><?php the_time('F j,Y'); ?> at <?php the_time('g:i a'); ?></small>
+    <small><?php the_category(' '); ?></small></br>
+    <small>Published on <?php the_time('F j,Y'); ?></small>
 </article>
