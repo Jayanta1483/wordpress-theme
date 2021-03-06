30fd736cf332e93ed80c16f8d4b3decb062f03ef
Jayanta <nemojoy2001@gmail.com>
2021-03-06 21:07:04 +0530

    setting tags, categories and post date

--- theme-2/single.php
+++ theme-2/single.php
@@ -1,4 +1,7 @@
 <?php get_header(); ?>
+<p class="text-center"><?php  the_tags('<span class="badge badge-light mr-2 ">','</span><span class="badge badge-light mr-2 text-white">' ,'</span>');  ?></p>
+<p class="text-center"><small>Posted on <?php the_time('F j,Y'); ?> at <?php the_time('g:i a'); ?>, in <?php the_category(' '); ?></small></p>
+<p class="text-center"><small><?php  edit_post_link();   ?></small></p>
 <div class="row">
     <div class="col-xs-12">
         <article>
