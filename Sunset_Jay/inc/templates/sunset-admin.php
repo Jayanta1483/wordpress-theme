75c4c7e33bb791a52e732a3d397f4d9b7ade2ac3
Jayanta <nemojoy2001@gmail.com>
2021-03-29 21:46:58 +0530

    style preview

--- Sunset_Jay/inc/templates/sunset-admin.php
+++ Sunset_Jay/inc/templates/sunset-admin.php
@@ -1,7 +1,24 @@
 <h1>Sunset Theme Options</h1>
-<?php settings_errors(  );    ?>
-<form action="options.php" method="post">
-<?php settings_fields( 'sunset-settings-group' );  ?>
-<?php do_settings_sections( 'jay-sunset' ); ?>
-<?php submit_button(  ); ?>
+<?php settings_errors();    ?>
+<?php
+$firstName = esc_attr(get_option('first_name'));
+$lastName = esc_attr(get_option('last_name'));
+$full_name = $firstName . ' ' . $lastName;
+$description = esc_attr(get_option('description'));
+?>
+<div class="preview-wrapper"><p style="text-align: center;"><i>Preview</i></p>
+<div class="sunset-sidebar-preview">
+    <div class="sunset-sidebar">
+        <h1 class="sunset-username"><?php echo $full_name; ?></h1>
+        <h2 class="sunset-description"><?php echo $description; ?></h2>
+        <div class="icons-wrapper">
+
+        </div>
+    </div>
+</div>
+</div>
+<form action="options.php" method="post" class="sunset-general-form">
+    <?php settings_fields('sunset-settings-group');  ?>
+    <?php do_settings_sections('jay-sunset'); ?>
+    <?php submit_button(); ?>
 </form>
