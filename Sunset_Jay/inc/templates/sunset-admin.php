2e10c0f82666533c1c6963160e6db88f193818dc
Jayanta <nemojoy2001@gmail.com>
2021-03-31 20:08:41 +0530

    profile picture functionality

--- Sunset_Jay/inc/templates/sunset-admin.php
+++ Sunset_Jay/inc/templates/sunset-admin.php
@@ -1,21 +1,26 @@
 <h1>Sunset Theme Options</h1>
 <?php settings_errors();    ?>
 <?php
+$picture = esc_attr(get_option('profile_picture'));
 $firstName = esc_attr(get_option('first_name'));
 $lastName = esc_attr(get_option('last_name'));
 $full_name = $firstName . ' ' . $lastName;
 $description = esc_attr(get_option('description'));
 ?>
-<div class="preview-wrapper"><p style="text-align: center;"><i>Preview</i></p>
-<div class="sunset-sidebar-preview">
-    <div class="sunset-sidebar">
-        <h1 class="sunset-username"><?php echo $full_name; ?></h1>
-        <h2 class="sunset-description"><?php echo $description; ?></h2>
-        <div class="icons-wrapper">
+<div class="preview-wrapper">
+    <p style="text-align: center;"><i>Preview</i></p>
+    <div class="sunset-sidebar-preview">
+        <div class="sunset-sidebar">
+            <div class="image-container">
+                <div class="profile-picture" style="background-image: url('<?php echo $picture; ?>');"></div>
+            </div>
+            <h1 class="sunset-username"><?php echo $full_name; ?></h1>
+            <h2 class="sunset-description"><?php echo $description; ?></h2>
+            <div class="icons-wrapper">
 
+            </div>
         </div>
     </div>
-</div>
 </div>
 <form action="options.php" method="post" class="sunset-general-form">
     <?php settings_fields('sunset-settings-group');  ?>
