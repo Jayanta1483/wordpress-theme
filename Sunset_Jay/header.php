ec21a3150d4c5eaa8808f6e075d77568add73177
Jayanta <nemojoy2001@gmail.com>
2021-05-31 17:16:32 +0530

    created header section

--- Sunset_Jay/header.php
+++ Sunset_Jay/header.php
@@ -7,13 +7,38 @@ This is Template for Header
 */
 ?>
 <!DOCTYPE html>
-<html lang=<?php language_attributes(  );  ?>>
+<html lang=<?php language_attributes();  ?>>
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <?php wp_head(); ?>
 </head>
-<body <?php body_class(); ?>>
-    
 
+<body <?php body_class(); ?>>
+    <header class="container-fluid px-0">
+        <div class="row no-gutters">
+            <div class="col-xl-12">
+                <div class="header-container">
+                    <div class="header-content bg-image text-center" style="background-image: url('<?php header_image(); ?>');">
+                    </div>
+                    <!--header-content-->
+                    <div class="header-text">
+                        <h2><?php bloginfo('name'); ?></h1>
+                            <h6><?php bloginfo('description'); ?>
+                        </h2>
+                    </div>
+                    <!--header-text-->
+                    <div class="nav-container">
+
+                    </div>
+                    <!--nav-container-->
+                </div>
+                <!--header-container-->
+            </div>
+            <!--col-xs-12-->
+        </div>
+        <!--row-->
+    </header>
+    <!--.container-fluid-->
